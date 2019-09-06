d98e0a090d1ea6b74b3e7875d7c08d7cee533c1a
[forrest kim] <[f1kim@ucsd.edu]>
2019-09-06 13:47:15 -0700

    changed static root

--- docs/conf.py
+++ docs/conf.py
@@ -71,6 +71,6 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-html_css_files = ["css/extra.css"]
+html_static_path = ['/']
+html_css_files = ["/css/extra.css"]
 html_js_files = ['js/extra.js']
